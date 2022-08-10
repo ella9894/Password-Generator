cdb7674414a02eaab664259664bc86b940dd6c98
Gabriella_Akpo <ella9894@gmail.com>
2022-08-10 13:19:36 -0400

    adding screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z'];
@@ -74,7 +73,7 @@ function generatePassword(){
     actualChars.push(getRandom(specialChars));
   }
 
-  for (var i = 0; i < passOptions.length; i++){
+  for (var i = 0; i < passOptions.passLength; i++){
     result.push(getRandom(probChars));
   }
   for (var i = 0; i < actualChars.length; i++){
